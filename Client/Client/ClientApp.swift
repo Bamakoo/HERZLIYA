a80d44732c3a4abeb41ca19f5aecaca075e411d3
Emmable <emma.gaubert@datadome.co>
2023-02-17 14:29:43 +0100

    adding the iOS app

---
+++ Client/Client/ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientApp.swift
+//  Client
+//
+//  Created by Emma Gaubert on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
