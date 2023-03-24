1831fc92ac58a39cdf1857315a0077d926e49178
Emmable <emma.gaubert@datadome.co>
2023-03-24 17:30:32 +0100

    deleting some useless print statements

--- Server/Sources/App/Authenticators/BasicAuthenticator.swift
+++ Server/Sources/App/Authenticators/BasicAuthenticator.swift
@@ -8,9 +8,7 @@ struct BasicUserAuthenticator: AsyncBasicAuthenticator {
         for request: Request
     ) async throws {
         let user = try request.content.decode(User.self)
-        print(user)
         let passwordHashesMatch = try user.verify(password: basic.password)
-        print(passwordHashesMatch, basic.password, basic.username)
         if basic.username == user.username && passwordHashesMatch {
             request.auth.login(user)
         } else {
