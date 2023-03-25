396e4ed6c53621df55ab6fe643878b25bec90dc1
Emmable <emma.gaubert@datadome.co>
2023-03-25 20:10:40 +0100

    removing the validation for the password hash

--- Server/Sources/App/Validations/UserValidations.swift
+++ Server/Sources/App/Validations/UserValidations.swift
@@ -4,6 +4,6 @@ extension User.Create: Validatable {
     static func validations(_ validations: inout Validations) {
         validations.add("username", as: String.self, is: !.empty)
         validations.add("email", as: String.self, is: .email)
-        validations.add("password_hash", as: String.self, is: .count(8...))
+        // TODO: more validations for all tables
     }
 }
