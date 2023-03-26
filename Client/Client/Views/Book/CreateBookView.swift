992eea42bc4e20067e8f199eebe5597d4623869f
Emmable <emma.gaubert@datadome.co>
2023-03-26 19:18:42 +0200

    incapsulating all the logic in a form

--- Client/Client/Views/Book/CreateBookView.swift
+++ Client/Client/Views/Book/CreateBookView.swift
@@ -9,34 +9,35 @@ import Foundation
 import SwiftUI
 
 struct CreateBookView: View {
-
+    
     @ObservedObject var viewModel = BooksViewModel(networkManager: BooksNetworkManager(httpClient: Networking()))
     var body: some View {
-        TextField("BookTitle", text: $viewModel.title, prompt: Text("Book's title"))
+        Form {
+            TextField("BookTitle", text: $viewModel.title, prompt: Text("Book's title"))
                 .background(Color.white)
-        Picker("Genre", selection: $viewModel.genre) {
-            ForEach(BookGenre.allCases) { genre in
-                Text(genre.rawValue.capitalized)
+            Picker("Genre", selection: $viewModel.genre) {
+                ForEach(BookGenre.allCases) { genre in
+                    Text(genre.rawValue.capitalized)
+                }
             }
-        }
-        Picker("State", selection: $viewModel.state) {
-            ForEach(BookState.allCases) { state in
-                Text(state.rawValue.capitalized)
+            Picker("State", selection: $viewModel.state) {
+                ForEach(BookState.allCases) { state in
+                    Text(state.rawValue.capitalized)
+                }
             }
-        }
-        TextField("BookPrice", value: $viewModel.price, format: .currency(code: "USD"), prompt: Text("Book's price"))
+            TextField("BookPrice", value: $viewModel.price, format: .currency(code: "USD"), prompt: Text("Book's price"))
                 .background(Color.white)
                 .keyboardType(.decimalPad)
-        TextField("Author", text: $viewModel.author, prompt: Text("Author's Name"))
+            TextField("Author", text: $viewModel.author, prompt: Text("Author's Name"))
                 .background(Color.white)
-        TextField("Description", text: $viewModel.description, prompt: Text("Description"))
-            Button {
-                Task {
-                    try await viewModel.createBook()
-                }
-            } label: {
-                Text("Offer book for sale")
+            TextField("Description", text: $viewModel.description, prompt: Text("Description"))
+        }
+        Button {
+            Task {
+                try await viewModel.createBook()
             }
-        
+        } label: {
+            Text("Offer book for sale")
+        }
     }
 }
