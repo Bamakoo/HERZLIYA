71a0f8a2a7243a0191fa4efe2128fa4b16dd6e9a
Emmable <emma.gaubert@datadome.co>
2023-04-04 17:56:33 +0200

    adding a search bar to NavigationSplitView

--- Client/Client/Views/Book/BooksList.swift
+++ Client/Client/Views/Book/BooksList.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct BooksList: View {
     @StateObject private var viewModel = BooksViewModel(networkManager: BooksNetworkManager(httpClient: Networking()))
     @State private var selection: Book?
+    @State private var searchText: String = ""
+    
     var body: some View {
         NavigationSplitView {
             List(viewModel.books, selection: $selection) { book in
@@ -26,6 +28,7 @@ struct BooksList: View {
                 Text("Pick a book")
             }
         }
+        .searchable(text: $searchText, prompt: "Search for your favorite book")
         .onAppear {
             Task {
                 await viewModel.fetchBooks()
