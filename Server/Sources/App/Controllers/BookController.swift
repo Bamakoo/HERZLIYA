8c3f97c97c9642d22df607979c4cc1201d50a9ee
Emmable <emma.gaubert@datadome.co>
2023-08-03 11:08:12 +0200

    now possible to chain search and filter queries

--- Server/Sources/App/Controllers/BookController.swift
+++ Server/Sources/App/Controllers/BookController.swift
@@ -10,11 +10,6 @@ struct BookController: RouteCollection {
         let tokenAuthenticator = UserToken.authenticator()
         let tokenMiddleware = UserToken.guardMiddleware()
         let tokenAuth = bookRoutes.grouped(tokenAuthenticator, tokenMiddleware)
-        tokenAuth.get("bought", use: getUserBoughtBooks)
-        tokenAuth.get("kart", use: getBooksInKart)
-        tokenAuth.get("likes", use: getUsersLikedBooks)
-        tokenAuth.get("sold", use: getUserSoldBooks)
-        tokenAuth.get("favorite-author", use: getMyFavoriteAuthorsBooks)
         tokenAuth.post(use: create)
         tokenAuth.patch(use: update)
         tokenAuth.patch(":bookID", "purchase", use: purchase)
@@ -45,140 +40,77 @@ struct BookController: RouteCollection {
     /// <#Description#>
     /// - Parameter req: <#req description#>
     /// - Returns: <#description#>
-    func sort(req: Request, searchBy: String, searchBool: Bool) async throws -> [GetBook] {
+    func sort(req: Request, searchBy: String, searchBool: Bool) async throws -> [Book] {
         do {
             switch (searchBy, searchBool) {
             case (SortBy.title.rawValue, true):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$title, .ascending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.title.rawValue, false):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$title, .descending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.state.rawValue, true):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$state, .ascending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.state.rawValue, false):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$state, .descending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.price.rawValue, true):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$price, .ascending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.price.rawValue, false):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$price, .descending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.genre.rawValue, true):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$genre, .ascending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.genre.rawValue, false):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$genre, .descending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.author.rawValue, true):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$author, .ascending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.author.rawValue, false):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$author, .descending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.date.rawValue, true):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$createdAt, .ascending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             case (SortBy.date.rawValue, false):
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .sort(\.$createdAt, .descending)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             default:
                 print("DEFAULTED")
-                let books = try await Book.query(on: req.db)
+                return try await Book.query(on: req.db)
                     .filter(\.$status == .available)
                     .all()
-                return try books.map { book in
-                    try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-                }
             }
         } catch {
             throw Abort(.badRequest)
-        }
-    }
-    
-    /// This function returns all the books in a user's kart
-    /// - Parameter req: the incoming GET request
-    /// - Returns: all the books a user has added to her kart
-    func getBooksInKart (req: Request) async throws -> [GetBook] {
-        /// get the user's ID
-        let user = try req.auth.require(User.self)
-        guard let userID = user.id else {
-            throw Abort(.badRequest, reason: "unable to get user")
-        }
-        
-        guard let kart = try await Kart.query(on: req.db)
-            .filter(\.$user.$id == userID)
-            .first()
-        else {
-            throw Abort(.notFound, reason: "unable to find kart")
-        }
-        
-        /// return all the books associated to the kart
-        let books = try await kart.$books.get(on: req.db)
-        return try books.map { book in
-            try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
         }
     }
     
@@ -188,42 +120,6 @@ struct BookController: RouteCollection {
     func getUsersLikedBooks(req: Request) async throws -> [GetBook] {
         let user = try req.auth.require(User.self)
         let books = try await user.$books.get(on: req.db)
-        return try books.map { book in
-            try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-        }
-    }
-    
-    /// The function that handles all GET requests to the /favorite-authors-books/:userID endpoint
-    /// - Parameter req: the incoming HTTP request
-    /// - Returns: an array of books written by a user's favorite author
-    func getMyFavoriteAuthorsBooks(req: Request) async throws -> [GetBook] {
-        let user = try req.auth.require(User.self)
-        let books = try await Book.query(on: req.db).group(.and) { group in
-            group.filter(\.$author == user.favoriteAuthor)
-                .filter(\.$status == .available)
-        } .all()
-        return try books.map { book in
-            try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-        }
-    }
-    
-    /// The function called by the controller when the /book/:userID route is called
-    /// - Parameter req: the incoming request, sent from the Client to the Server
-    /// - Returns: An array of books the user has purchased
-    func getUserBoughtBooks(req: Request) async throws -> [GetBook] {
-        let user = try req.auth.require(User.self)
-        let books = try await user.$baughtBooks.get(on: req.db)
-        return try books.map { book in
-            try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-        }
-    }
-    
-    /// The function called by the controller when the /book/:userID route is called
-    /// - Parameter req: the incoming request, received by the server
-    /// - Returns: an array of Book objects the user has sold
-    func getUserSoldBooks(req: Request) async throws -> [GetBook] {
-        let user = try req.auth.require(User.self)
-        let books = try await user.$soldBooks.get(on: req.db)
         return try books.map { book in
             try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
         }
@@ -242,30 +138,34 @@ struct BookController: RouteCollection {
     /// <#Description#>
     /// - Parameter req: <#req description#>
     /// - Returns: <#description#>
-    func searchHandler(req: Request, searchTerm: String) async throws -> [GetBook] {
-        let books = try await Book.query(on: req.db).group(.or) { group in
+    func searchHandler(req: Request, searchTerm: String) async throws -> [Book] {
+        return try await Book.query(on: req.db).group(.or) { group in
             group.filter(\.$title =~ searchTerm)
                 .filter(\.$author =~ searchTerm)
         }
-            .filter(\.$status == .available)
-            .all()
-        return try books.map { book in
-            try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
-        }
+        .filter(\.$status == .available)
+        .all()
     }
+    
+    // TODO: it is possible to chain search, filter AND sort
+    // TODO: sort can take an optional Array of Getbooks and return an array of books
     
     /// <#Description#>
     /// - Parameter req: <#req description#>
     /// - Returns: <#description#>
     func index(req: Request) async throws -> [GetBook] {
         do {
+            // MARK:
+            // instantiate an empty array of Book objects
+            var books = [Book]()
+            
+            // MARK: a constant to house all the queries added to the URLRequest
             let queryItems = try req.query.decode(Book.QueryFilter.self)
-            if let searchTerm = queryItems.search {
-                // TODO: it is possible to chain search, filter AND sort
-                return try await searchHandler(req: req, searchTerm: searchTerm)
-            }
-            let books = try await Book.query(on: req.db).group(.and) { group in
+            
+            // MARK: Filtering the books
+            books = try await Book.query(on: req.db).group(.and) { group in
                 if let genre = queryItems.genre {
+                    print(genre)
                     group.filter(\.$genre == genre)
                 }
                 if let state = queryItems.state {
@@ -280,16 +180,25 @@ struct BookController: RouteCollection {
                 if let price = queryItems.price {
                     group.filter(\.$price == price)
                 }
+                
+                // MARK: searching for specific books in the filtered array of books
+                if let searchTerm = queryItems.search {
+                    print(searchTerm)
+                    group.filter(\.$title =~ searchTerm)
+                }
+                
+                // MARK: filter all the books so only the avaialble ones are left
                 group.filter(\.$status == .available)
             } .all()
+            
+            // MARK: sort
             if let sortBoolean = queryItems.sort {
                 switch sortBoolean {
                 case true:
                     if let sortBy = queryItems.by,
-                       let ascendingBool = queryItems.ascending
-                    {
+                       let ascendingBool = queryItems.ascending {
                         // TODO: sort can take an optional Array of Getbooks and return an array of books
-                        return try await sort(req: req, searchBy: sortBy, searchBool: ascendingBool)
+                        books = try await sort(req: req, searchBy: sortBy, searchBool: ascendingBool)
                     }
                 case false:
                     return try books.map { book in
@@ -301,6 +210,8 @@ struct BookController: RouteCollection {
                     }
                 }
             }
+            
+            // MARK: map results to the DTO and return
             return try books.map { book in
                 try GetBook(id: book.requireID(), title: book.title, author: book.author, price: book.price, state: book.state)
             }
