280ac4c314f59483c064e817929caff6348c2b89
O-drey <98774051+O-drey@users.noreply.github.com>
2023-09-01 09:59:10 +0200

    ADD : node_modules to Express gitignore DEV : POST books w/o token DEV : retrieve user

--- Vue/src/libs/interfaces/books.ts
+++ Vue/src/libs/interfaces/books.ts
@@ -12,4 +12,5 @@ export interface Books {
     | 'good'
     | 'excellent'
     | 'brandNew'
+    | null
 }
--- Vue/src/libs/ui/TwInputText.vue
+++ Vue/src/libs/ui/TwInputText.vue
@@ -23,7 +23,7 @@
       :disabled="disabled"
       :step="step"
       :min="min"
-      class="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-secondary-dark sm:text-sm sm:leading-6"
+      class="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-secondary-dark sm:text-sm sm:leading-6 px-2"
     />
   </label>
 </template>
