52f04902ff4bf8b295eb225d874801c4220bb11e
O-drey <98774051+O-drey@users.noreply.github.com>
2023-09-22 15:52:58 +0200

    ADD : token token variable on create books

--- Vue/src/api/fetchs/useFetchBooks.ts
+++ Vue/src/api/fetchs/useFetchBooks.ts
@@ -13,8 +13,9 @@ export const useFetchBooks = () => {
   }
 
   const create = async (datas: Omit<Books, 'id'>) => {
+    const loginCredentials = window.localStorage.getItem('token')
     const { data } = await httpClient.post<Books>('/books', datas, {
-      headers: { Authorization: 'Bearer TRbHQ7EMy/fk8uuWto9zfA==' }
+      headers: { Authorization: `Basic ${loginCredentials}` }
     })
     return data
   }
